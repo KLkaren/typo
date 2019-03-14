ebae4904e646e940d39b2349b4439d5b2410e1bb
EC2 Default User <ec2-user@ip-172-31-22-176.us-east-2.compute.internal>
2019-03-14 19:10:29 +0000

    Added Rspec

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -16,6 +16,27 @@ describe Admin::CategoriesController do
     assert_response :redirect, :action => 'index'
   end
 
+   it "test_create" do
+    ct = Factory(:category)
+    Category.should_receive(:find).with(:all).and_return([])
+    Category.should_receive(:new).and_return(ct)
+    ct.should_receive(:save!).and_return(true)
+    post :edit, 'category' => { :name => "new category" }
+    assert_response :redirect
+    assert_redirected_to :action => 'index'
+  end
+
+  describe "test_new" do
+    before(:each) do
+      get :new
+    end
+    
+     it 'should render template new' do
+      assert_template 'new'
+    end
+
+  end
+  
   describe "test_edit" do
     before(:each) do
       get :edit, :id => Factory(:category).id
